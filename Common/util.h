8d7dd655be4519d5c83a730d610c8dc23b03b0c7
Sam Caulfield <sam@samcaulfield.com>
2014-05-28 15:23:10 +0100

    Moved window dimensions to the common header file.

--- Common/util.h
+++ Common/util.h
@@ -3,6 +3,11 @@
 
 GLuint program_object;
 
+/* The dimensions of the window. */
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
+
 /* Initialises glut, glew and the shaders. */
 void init (int *argc, char **argv, char *window_title);
 
